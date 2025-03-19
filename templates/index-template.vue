347202471b7e6100b5bdb0ddbb9ce65b88862192
Michael Thiessen <electromikenetic@gmail.com>
2025-03-19 15:03:54 -0400

    chore: add template file

---
+++ templates/index-template.vue
@@ -0,0 +1,18 @@
+<style scoped>
+.page-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.title {
+  font-size: 2.25rem;
+  font-weight: bold;
+}
+
+.button {
+  margin-top: 1rem;
+}
+</style>
